3b005731ce455d64a080ce8a079e46ef23a3e2a6
ChrisKellFSD <chriskellcsn@gmail.com>
2022-07-03 15:56:00 +0000

    Add the 10 questions to my quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,104 @@
+//Questions for the quiz
+
+const questions = [
+    {
+        question: "The Man of Steel is another name for which superhero?",
+        answers: {
+          a: "Batman",
+          b: "Superman",
+          c: "Iron Man",
+          d: "Aquaman"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
+        answers: {
+          a: "Catwoman",
+          b: "Wonder Woman",
+          c: "Super Girl",
+          d: "The Wasp"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which superhero cannot transform back into the human form anymore?",
+        answers: {
+          a: "The Incredible Hulk",
+          b: "Ant-Man",
+          c: "The Thing",
+          d: "The Human Torch"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "What villain got his distinctive appearance from toxic chemicals at a plant?",
+        answers: {
+          a: "Joker",
+          b: "Doomsday",
+          c: "Two-Face",
+          d: "The Penguin"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Which superhero is blinded by radioactive components and nicknamed the Man without fear?",
+        answers: {
+          a: "Green Lantern",
+          b: "Daredevil",
+          c: "Wolverine",
+          d: "The Incredible Hulk"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "The Green Lantern gains his power from which object?",
+        answers: {
+          a: "A Ring",
+          b: "Glasses",
+          c: "A Sword",
+          d: "A Gun"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Which newspaper does Spiderman/Peter Parker work for?",
+        answers: {
+          a: "The Daily Bugle",
+          b: "The Daily Gothamite",
+          c: "The Daily Planet",
+          d: "The Daily Star"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "What is the name of the archnemesis of the Fantastic Four?",
+        answers: {
+          a: "Llan the Sorcerer",
+          b: "Count Nefaria",
+          c: "The Mandarin",
+          d: "Dr. Doom"
+        },
+        correctAnswer: "d"
+      },
+      {
+        question: "What are the claws of Wolverine made of?",
+        answers: {
+          a: "Aluminium",
+          b: "Adamantium",
+          c: "Titanium",
+          d: "Vibranium"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which of these X-Men DO NOT have either blue skin or fur",
+        answers: {
+          a: "Cyclops",
+          b: "Nightcrawler",
+          c: "Beast",
+          d: "Mystique"
+        },
+        correctAnswer: "a"
+      }
+]
