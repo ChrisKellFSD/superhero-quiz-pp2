1d984f3cfc99b0f84deae87ae9ac89fa5d024b0c
ChrisKellFSD <chriskellcsn@gmail.com>
2022-07-05 18:20:45 +0000

    Add replay button so quiz can be restarted

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,184 +1,164 @@
 (function(){
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
+  function startQuiz(){
+
+    const output = [];
+
+    quizQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+         const answers = [];
+
+        for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
           );
         }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+    quizContainer.innerHTML = output.join('');
+  }
 
-    const myQuestions = [
+  function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    quizQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = 'green';
+
+      } else{
+
+        answerContainers[questionNumber].style.color = 'red';
+
+      }
+    });
+    resultsContainer.innerHTML = `You got ${numCorrect} out of ${quizQuestions.length}!`;
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const replayButton = document.getElementById('replay');
+
+  const quizQuestions = [
+  {
+      question: "The Man of Steel is another name for which superhero?",
+      answers: {
+        A: "Batman",
+        B: "Superman",
+        C: "Iron Man",
+        D: "Aquaman"
+      },
+      correctAnswer: "B"
+    },
     {
-        question: "The Man of Steel is another name for which superhero?",
-        answers: {
-          a: "Batman",
-          b: "Superman",
-          c: "Iron Man",
-          d: "Aquaman"
-        },
-        correctAnswer: "b"
+      question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
+      answers: {
+        A: "Catwoman",
+        B: "Wonder Woman",
+        C: "Super Girl",
+        D: "The Wasp"
       },
-      {
-        question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
-        answers: {
-          a: "Catwoman",
-          b: "Wonder Woman",
-          c: "Super Girl",
-          d: "The Wasp"
-        },
-        correctAnswer: "b"
+      correctAnswer: "B"
+    },
+    {
+      question: "Which superhero cannot transform back into the human form anymore?",
+      answers: {
+        A: "The Incredible Hulk",
+        B: "Ant-Man",
+        C: "The Thing",
+        D: "The Human Torch"
       },
-      {
-        question: "Which superhero cannot transform back into the human form anymore?",
-        answers: {
-          a: "The Incredible Hulk",
-          b: "Ant-Man",
-          c: "The Thing",
-          d: "The Human Torch"
-        },
-        correctAnswer: "c"
+      correctAnswer: "C"
+    },
+    {
+      question: "What villain got his distinctive appearance from toxic chemicals at a plant?",
+      answers: {
+        A: "Joker",
+        B: "Doomsday",
+        C: "Two-Face",
+        D: "The Penguin"
       },
-      {
-        question: "What villain got his distinctive appearance from toxic chemicals at a plant?",
-        answers: {
-          a: "Joker",
-          b: "Doomsday",
-          c: "Two-Face",
-          d: "The Penguin"
-        },
-        correctAnswer: "a"
+      correctAnswer: "A"
+    },
+    {
+      question: "Which superhero is blinded by radioactive components and nicknamed the Man without fear?",
+      answers: {
+        A: "Green Lantern",
+        B: "Daredevil",
+        C: "Wolverine",
+        D: "The Incredible Hulk"
       },
-      {
-        question: "Which superhero is blinded by radioactive components and nicknamed the Man without fear?",
-        answers: {
-          a: "Green Lantern",
-          b: "Daredevil",
-          c: "Wolverine",
-          d: "The Incredible Hulk"
-        },
-        correctAnswer: "b"
+      correctAnswer: "B"
+    },
+    {
+      question: "The Green Lantern gains his power from which object?",
+      answers: {
+        A: "A Ring",
+        B: "Glasses",
+        C: "A Sword",
+        D: "A Gun"
       },
-      {
-        question: "The Green Lantern gains his power from which object?",
-        answers: {
-          a: "A Ring",
-          b: "Glasses",
-          c: "A Sword",
-          d: "A Gun"
-        },
-        correctAnswer: "a"
+      correctAnswer: "A"
+    },
+    {
+      question: "Which newspaper does Spiderman/Peter Parker work for?",
+      answers: {
+        A: "The Daily Bugle",
+        B: "The Daily Gothamite",
+        C: "The Daily Planet",
+        D: "The Daily Star"
       },
-      {
-        question: "Which newspaper does Spiderman/Peter Parker work for?",
-        answers: {
-          a: "The Daily Bugle",
-          b: "The Daily Gothamite",
-          c: "The Daily Planet",
-          d: "The Daily Star"
-        },
-        correctAnswer: "a"
+      correctAnswer: "A"
+    },
+    {
+      question: "What is the name of the archnemesis of the Fantastic Four?",
+      answers: {
+        A: "Llan the Sorcerer",
+        B: "Count Nefaria",
+        C: "The Mandarin",
+        D: "Dr. Doom"
       },
-      {
-        question: "What is the name of the archnemesis of the Fantastic Four?",
-        answers: {
-          a: "Llan the Sorcerer",
-          b: "Count Nefaria",
-          c: "The Mandarin",
-          d: "Dr. Doom"
-        },
-        correctAnswer: "d"
+      correctAnswer: "D"
+    },
+    {
+      question: "What are the claws of Wolverine made of?",
+      answers: {
+        A: "Aluminium",
+        B: "Adamantium",
+        C: "Titanium",
+        D: "Vibranium"
       },
-      {
-        question: "What are the claws of Wolverine made of?",
-        answers: {
-          a: "Aluminium",
-          b: "Adamantium",
-          c: "Titanium",
-          d: "Vibranium"
-        },
-        correctAnswer: "b"
+      correctAnswer: "B"
+    },
+    {
+      question: "Which of these X-Men DO NOT have either blue skin or fur",
+      answers: {
+        A: "Cyclops",
+        B: "Nightcrawler",
+        C: "Beast",
+        D: "Mystique"
       },
-      {
-        question: "Which of these X-Men DO NOT have either blue skin or fur",
-        answers: {
-          a: "Cyclops",
-          b: "Nightcrawler",
-          c: "Beast",
-          d: "Mystique"
-        },
-        correctAnswer: "a"
-      }]
-  
-    // Kick things off
-    buildQuiz();
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-  })();
+      correctAnswer: "A"
+    }]
+
+  startQuiz();
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  replayButton.addEventListener('click', startQuiz);
+})();
