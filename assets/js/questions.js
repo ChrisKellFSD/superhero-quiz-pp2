1cb7845c078b367c1db5b6266eae527e186b338d
ChrisKellFSD <chriskellcsn@gmail.com>
2022-07-07 19:57:03 +0000

    Cleane up code and seperate questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,101 @@
+const quizQuestions = [
+    {
+        question: "The Man of Steel is another name for which superhero?",
+        answers: {
+          A: "Batman",
+          B: "Superman",
+          C: "Iron Man",
+          D: "Aquaman"
+        },
+        correctAnswer: "B"
+      },
+      {
+        question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
+        answers: {
+          A: "Catwoman",
+          B: "Wonder Woman",
+          C: "Super Girl",
+          D: "The Wasp"
+        },
+        correctAnswer: "B"
+      },
+      {
+        question: "Which superhero cannot transform back into the human form anymore?",
+        answers: {
+          A: "The Incredible Hulk",
+          B: "Ant-Man",
+          C: "The Thing",
+          D: "The Human Torch"
+        },
+        correctAnswer: "C"
+      },
+      {
+        question: "What villain got his distinctive appearance from toxic chemicals at a plant?",
+        answers: {
+          A: "Joker",
+          B: "Doomsday",
+          C: "Two-Face",
+          D: "The Penguin"
+        },
+        correctAnswer: "A"
+      },
+      {
+        question: "Which superhero is blinded by radioactive components and nicknamed the Man without fear?",
+        answers: {
+          A: "Green Lantern",
+          B: "Daredevil",
+          C: "Wolverine",
+          D: "The Incredible Hulk"
+        },
+        correctAnswer: "B"
+      },
+      {
+        question: "The Green Lantern gains his power from which object?",
+        answers: {
+          A: "A Ring",
+          B: "Glasses",
+          C: "A Sword",
+          D: "A Gun"
+        },
+        correctAnswer: "A"
+      },
+      {
+        question: "Which newspaper does Spiderman/Peter Parker work for?",
+        answers: {
+          A: "The Daily Bugle",
+          B: "The Daily Gothamite",
+          C: "The Daily Planet",
+          D: "The Daily Star"
+        },
+        correctAnswer: "A"
+      },
+      {
+        question: "What is the name of the archnemesis of the Fantastic Four?",
+        answers: {
+          A: "Llan the Sorcerer",
+          B: "Count Nefaria",
+          C: "The Mandarin",
+          D: "Dr. Doom"
+        },
+        correctAnswer: "D"
+      },
+      {
+        question: "What are the claws of Wolverine made of?",
+        answers: {
+          A: "Aluminium",
+          B: "Adamantium",
+          C: "Titanium",
+          D: "Vibranium"
+        },
+        correctAnswer: "B"
+      },
+      {
+        question: "Which of these X-Men DO NOT have either blue skin or fur",
+        answers: {
+          A: "Cyclops",
+          B: "Nightcrawler",
+          C: "Beast",
+          D: "Mystique"
+        },
+        correctAnswer: "A"
+      }]
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,15 +1,25 @@
+// Variables
 var timer;
 var timerText = document.getElementById('timer');
+var sec = 0;
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const replayButton = document.getElementById('replay');
+const previousButton = document.getElementById('previousQ');
+const nextButton = document.getElementById('nextQ');
 
+
+
+// Timer
 (function (){
-  var sec = 0;
   timer = setInterval(()=>{
     timerText.innerHTML = '00:'+sec;
     sec++;
   }, 1000)
 })()
 
-function startQuiz(){
+function makeQuiz(){
 
     const output = [];
 
@@ -61,117 +71,10 @@ function startQuiz(){
     resultsContainer.innerHTML = `You got ${numCorrect} out of ${quizQuestions.length}!`;
   }
 
-  const quizContainer = document.getElementById('quiz');
-  const resultsContainer = document.getElementById('results');
-  const submitButton = document.getElementById('submit');
-  const replayButton = document.getElementById('replay');
+  makeQuiz();
 
-  const quizQuestions = [
-  {
-      question: "The Man of Steel is another name for which superhero?",
-      answers: {
-        A: "Batman",
-        B: "Superman",
-        C: "Iron Man",
-        D: "Aquaman"
-      },
-      correctAnswer: "B"
-    },
-    {
-      question: "Which superhero has super tools such as the magic lasso and bullet-proof bracelets?",
-      answers: {
-        A: "Catwoman",
-        B: "Wonder Woman",
-        C: "Super Girl",
-        D: "The Wasp"
-      },
-      correctAnswer: "B"
-    },
-    {
-      question: "Which superhero cannot transform back into the human form anymore?",
-      answers: {
-        A: "The Incredible Hulk",
-        B: "Ant-Man",
-        C: "The Thing",
-        D: "The Human Torch"
-      },
-      correctAnswer: "C"
-    },
-    {
-      question: "What villain got his distinctive appearance from toxic chemicals at a plant?",
-      answers: {
-        A: "Joker",
-        B: "Doomsday",
-        C: "Two-Face",
-        D: "The Penguin"
-      },
-      correctAnswer: "A"
-    },
-    {
-      question: "Which superhero is blinded by radioactive components and nicknamed the Man without fear?",
-      answers: {
-        A: "Green Lantern",
-        B: "Daredevil",
-        C: "Wolverine",
-        D: "The Incredible Hulk"
-      },
-      correctAnswer: "B"
-    },
-    {
-      question: "The Green Lantern gains his power from which object?",
-      answers: {
-        A: "A Ring",
-        B: "Glasses",
-        C: "A Sword",
-        D: "A Gun"
-      },
-      correctAnswer: "A"
-    },
-    {
-      question: "Which newspaper does Spiderman/Peter Parker work for?",
-      answers: {
-        A: "The Daily Bugle",
-        B: "The Daily Gothamite",
-        C: "The Daily Planet",
-        D: "The Daily Star"
-      },
-      correctAnswer: "A"
-    },
-    {
-      question: "What is the name of the archnemesis of the Fantastic Four?",
-      answers: {
-        A: "Llan the Sorcerer",
-        B: "Count Nefaria",
-        C: "The Mandarin",
-        D: "Dr. Doom"
-      },
-      correctAnswer: "D"
-    },
-    {
-      question: "What are the claws of Wolverine made of?",
-      answers: {
-        A: "Aluminium",
-        B: "Adamantium",
-        C: "Titanium",
-        D: "Vibranium"
-      },
-      correctAnswer: "B"
-    },
-    {
-      question: "Which of these X-Men DO NOT have either blue skin or fur",
-      answers: {
-        A: "Cyclops",
-        B: "Nightcrawler",
-        C: "Beast",
-        D: "Mystique"
-      },
-      correctAnswer: "A"
-    }]
 
-  startQuiz();
-
-  const previousButton = document.getElementById('previousQ');
-  const nextButton = document.getElementById('nextQ');
+// Slider function to show previous and next questions
   const slides = document.querySelectorAll('.slider');
   let currentSlide = 0;
 
@@ -206,11 +109,12 @@ function startQuiz(){
   }
 
   function replayQuiz() {
-    startQuiz();
+    makeQuiz();
 }
 
-  // Event listeners
-  submitButton.addEventListener('click', showResults);
-  replayButton.addEventListener('click', replayQuiz);
-  previousButton.addEventListener('click', showPreviousSlide);
-  nextButton.addEventListener('click', showNextSlide);
+
+// Event listeners
+submitButton.addEventListener('click', showResults);
+replayButton.addEventListener('click', replayQuiz);
+previousButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
